ef8dddc52d0ea3a3487dba1e80d9ee9947115029
Asif Rahaman <asifrahaman162@gmail.com>
2025-01-14 15:34:56 +0530

    wip: add ui for employees dashboard

--- frontend/src/app/dashboard/employees/page.tsx
+++ frontend/src/app/dashboard/employees/page.tsx
@@ -26,10 +26,101 @@ function EmployeeList() {
           </button>
         </div>
       </div>
-      <div className="m-8 flex gap-4 h-1/4">
-        <div className="bg-white p-4 rounded-xl h-full w-1/5"></div>
-        <div className="bg-white p-4 rounded-xl h-full w-3/5"></div>
-        <div className="bg-white p-4 rounded-xl h-full w-2/5"></div>
+      <div className="m-8 flex gap-4 ">
+        <div className="bg-white p-4  py-6 rounded-xl h-full w-2/5 flex flex-col">
+          <div className="h-2/3 flex justify-between">
+            <div className="flex flex-col gap-2">
+              <div className="text-sm">Nationality</div>
+              <div className="font-medium text-2xl">25</div>
+              <div>Singaporeans</div>
+            </div>
+            <div>
+              <img src="/images/charts/pie.svg" alt="" className="h-28" />
+            </div>
+          </div>
+          <div className="h-1/3">
+            <div className="flex text-gray-600 items-center gap-4 flex-wrap p-4 rounded-md">
+              <div className="flex items-center">
+                <div className="w-1 h-6 bg-cyan-500 mr-2 rounded-full"></div>
+                <span className=" font-medium">25 Full-Timer</span>
+              </div>
+              <div className="flex items-center">
+                <div className="w-1 h-6 bg-yellow-500 mr-2 rounded-full"></div>
+                <span className=" font-medium">10 Part-Timer</span>
+              </div>
+              <div className="flex items-center">
+                <div className="w-1 h-6 bg-purple-500 mr-2 rounded-full"></div>
+                <span className=" font-medium">5 Contract</span>
+              </div>
+              <div className="flex items-center">
+                <div className="w-1 h-6 bg-gray-300 mr-2 rounded-full"></div>
+                <span className=" font-medium">6 Intern</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white p-4 rounded-xl py-8 h-full w-2/5">
+          <div className="h-2/3 flex justify-between">
+            <div className="flex flex-col gap-2">
+              <div className="text-sm">Nationality</div>
+              <div className="font-medium text-2xl">25</div>
+              <div>Singaporeans</div>
+            </div>
+          </div>
+
+          <div>
+            <img src="/images/charts/line.svg" alt="" />
+          </div>
+
+          <div className="h-1/3">
+            <div className="flex text-gray-600 items-center gap-4 flex-wrap p-4 rounded-md">
+              <div className="flex items-center">
+                <div className="w-1 h-6 bg-cyan-500 mr-2 rounded-full"></div>
+                <span className=" font-medium">25 Full-Timer</span>
+              </div>
+              <div className="flex items-center">
+                <div className="w-1 h-6 bg-yellow-500 mr-2 rounded-full"></div>
+                <span className=" font-medium">10 Part-Timer</span>
+              </div>
+              <div className="flex items-center">
+                <div className="w-1 h-6 bg-purple-500 mr-2 rounded-full"></div>
+                <span className=" font-medium">5 Contract</span>
+              </div>
+              <div className="flex items-center">
+                <div className="w-1 h-6 bg-gray-300 mr-2 rounded-full"></div>
+                <span className=" font-medium">6 Intern</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white p-4 rounded-xl h-full w-2/5">
+          <div className="h-2/3 flex justify-between">
+            <div className="flex flex-col gap-2">
+              <div className="text-sm">Nationality</div>
+              <div className="font-medium text-2xl">25</div>
+              <div>Singaporeans</div>
+            </div>
+            <div>
+              <img src="/images/charts/halfPie.svg" alt="" className="h-22" />
+            </div>
+          </div>
+          <div className="h-1/3">
+            <div className="flex text-gray-600 items-center gap-4 flex-wrap p-4 rounded-md">
+              <div className="flex items-center">
+                <div className="w-1 h-6 bg-cyan-500 mr-2 rounded-full"></div>
+                <span className=" font-medium">25 Full-Timer</span>
+              </div>
+              <div className="flex items-center">
+                <div className="w-1 h-6 bg-yellow-500 mr-2 rounded-full"></div>
+                <span className=" font-medium">10 Part-Timer</span>
+              </div>
+              <div className="flex items-center">
+                <div className="w-1 h-6 bg-purple-500 mr-2 rounded-full"></div>
+                <span className=" font-medium">5 Contract</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="flex flex-col"></div>
       <div className="container mx-auto p-4">
