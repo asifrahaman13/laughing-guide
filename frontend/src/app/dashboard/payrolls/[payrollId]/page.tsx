a57fcd48e328cda3fa3a041438c1037293741334
Asif Rahaman <asifrahaman162@gmail.com>
2025-01-15 03:10:05 +0530

    fix: error in payroll id

--- frontend/src/app/dashboard/payrolls/[payrollId]/page.tsx
+++ frontend/src/app/dashboard/payrolls/[payrollId]/page.tsx
@@ -3,11 +3,11 @@ import { useParams } from "next/navigation";
 import React from "react";
 
 export default function Page() {
-  const params = useParams<{ payrollid: string }>();
+  const params = useParams<{ payrollId: string }>();
 
   return (
     <div>
-      <h1>{params.payrollid}</h1>
+      <h1>Person with id {params.payrollId}</h1>
     </div>
   );
 }
