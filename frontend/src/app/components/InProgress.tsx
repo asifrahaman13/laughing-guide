80860473b7da1e820fa283ddd2faf6f3a49841c7
Asif Rahaman <asifrahaman162@gmail.com>
2025-01-15 15:47:00 +0530

    wip: dynamic message for page not found

--- frontend/src/app/components/InProgress.tsx
+++ frontend/src/app/components/InProgress.tsx
@@ -1,7 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 
-export default function InProgress() {
+type InProgressProps = {
+  title?: string;
+  message?: string;
+};
+
+export default function InProgress({ title, message }: InProgressProps) {
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-center">
@@ -11,11 +16,11 @@ export default function InProgress() {
           className="mb-8"
         />
         <h1 className="text-2xl font-bold text-gray-800 mb-4">
-          Sorry, this page is under development!
+          {title || "Under Development"}
         </h1>
         <p className="text-gray-600">
-          We are working hard to bring this page to you soon. Please check back
-          later.
+          {message ||
+            "This page is under development. Please check back later."}
         </p>
       </div>
     </>
--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import InProgress from "./components/InProgress";
 
 export default function Custom404() {
-  return <h1>Sorry something the page does not exist</h1>;
+  return (
+    <>
+      <InProgress
+        title="No page found"
+        message="Sorry this page does not exists Please visit our home page"
+      />
+    </>
+  );
 }
