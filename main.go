7466213336ed8139a24b18cacaaddaf0f8161839
Asif Rahaman <asifrahaman162@gmail.com>
2025-01-16 19:55:09 +0530

    wip: centralized config file

--- main.go
+++ main.go
@@ -1,105 +1,89 @@
 package main
 
 import (
-	"context"
-	"database/sql"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
+    "context"
+    "database/sql"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "os/signal"
+    "syscall"
+    "time"
 
-	"github.com/asifrahaman13/laughing-guide/src/core/service"
-	"github.com/asifrahaman13/laughing-guide/src/database"
-
-	"github.com/asifrahaman13/laughing-guide/src/handler"
-	"github.com/asifrahaman13/laughing-guide/src/repository"
-	"github.com/asifrahaman13/laughing-guide/src/routers"
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
+    "github.com/asifrahaman13/laughing-guide/src/config"
+    "github.com/asifrahaman13/laughing-guide/src/core/service"
+    "github.com/asifrahaman13/laughing-guide/src/database"
+    "github.com/asifrahaman13/laughing-guide/src/handler"
+    "github.com/asifrahaman13/laughing-guide/src/repository"
+    "github.com/asifrahaman13/laughing-guide/src/routers"
+    "github.com/gin-contrib/cors"
+    "github.com/gin-gonic/gin"
+    _ "github.com/lib/pq"
 )
 
 func main() {
+    database.InitDB()
 
-	database.InitDB()
+    cfg, err := config.LoadConfig()
+    if err != nil {
+        log.Fatalf("Error loading config: %v", err)
+    }
 
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found, using default environment variables")
-	}
-	mode := os.Getenv("GIN_MODE")
-	fmt.Println("Mode: ", mode)
-	if mode == "" {
-		mode = "debug"
-	}
+    if cfg.GinMode == "" {
+        cfg.GinMode = "debug"
+    }
 
-	if mode == "release" {
-		gin.SetMode(gin.ReleaseMode)
-	} else {
-		gin.SetMode(gin.DebugMode)
-	}
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found, using default environment variables")
-	}
+    if cfg.GinMode == "release" {
+        gin.SetMode(gin.ReleaseMode)
+    } else {
+        gin.SetMode(gin.DebugMode)
+    }
 
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbSSLMode := os.Getenv("DB_SSLMODE")
+    databaseURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s", cfg.DBUser, cfg.DBPassword, cfg.DBHost, cfg.DBPort, cfg.DBName, cfg.DBSSLMode)
 
-	databaseURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s", dbUser, dbPassword, dbHost, dbPort, dbName, dbSSLMode)
+    db, err := sql.Open("postgres", databaseURL)
+    if err != nil {
+        log.Fatal(err)
+    }
+    employeeRepo := repository.NewEmployeeRepository(db)
+    employeeService := service.NewEmployeeService(employeeRepo)
+    employeeHandler := handler.NewEmployeeHandler(employeeService)
 
-	db, err := sql.Open("postgres", databaseURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-	employeeRepo := repository.NewEmployeeRepository(db)
-	employeeService := service.NewEmployeeService(employeeRepo)
-	employeeHandler := handler.NewEmployeeHandler(employeeService)
+    fileService := service.NewFileService(employeeRepo)
+    fileHandler := handler.NewFileHandler(fileService)
 
-	fileService := service.NewFileService(employeeRepo)
-	fileHandler := handler.NewFileHandler(fileService)
+    r := gin.Default()
 
-	r := gin.Default()
+    r.Use(cors.Default())
 
-	r.Use(cors.Default())
+    r.GET("/health", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "message": "ok",
+        })
+    })
 
-	r.GET("/health", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "ok",
-		})
-	})
+    r = routers.InitRoutes(employeeHandler, fileHandler)
+    srv := &http.Server{
+        Addr:    ":" + cfg.Port,
+        Handler: r,
+    }
 
-	r = routers.InitRoutes(employeeHandler, fileHandler)
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8000"
-	}
-	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: r,
-	}
+    go func() {
+        log.Println("Server started on :" + cfg.Port)
+        if err := srv.ListenAndServe(); err != nil {
+            log.Fatalf("Failed to start server: %v", err)
+        }
+    }()
 
-	go func() {
-		log.Println("Server started on :" + port)
-		if err := srv.ListenAndServe(); err != nil {
-			log.Fatalf("Failed to start server: %v", err)
-		}
-	}()
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	<-c
-	log.Println("Shutting down server...")
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatalf("Server forced to shutdown: %v", err)
-	}
-	log.Println("Server exiting")
+    c := make(chan os.Signal, 1)
+    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+    <-c
+    log.Println("Shutting down server...")
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+    if err := srv.Shutdown(ctx); err != nil {
+        log.Fatalf("Server forced to shutdown: %v", err)
+    }
+    log.Println("Server exiting")
 }
--- src/config/config.go
+++ src/config/config.go
@@ -1,5 +1,42 @@
 package config
 
-func Config() {
+import (
+    "log"
+    "os"
 
+    "github.com/joho/godotenv"
+)
+
+type Config struct {
+    DBUser     string
+    DBPassword string
+    DBName     string
+    DBHost     string
+    DBPort     string
+    DBSSLMode  string
+    GinMode    string
+    Port       string
+}
+
+func LoadConfig() (*Config, error) {
+    if err := godotenv.Load(); err != nil {
+        log.Println("No .env file found, using default environment variables")
+    }
+
+    config := &Config{
+        DBUser:     os.Getenv("DB_USER"),
+        DBPassword: os.Getenv("DB_PASSWORD"),
+        DBName:     os.Getenv("DB_NAME"),
+        DBHost:     os.Getenv("DB_HOST"),
+        DBPort:     os.Getenv("DB_PORT"),
+        DBSSLMode:  os.Getenv("DB_SSLMODE"),
+        GinMode:    os.Getenv("GIN_MODE"),
+        Port:       os.Getenv("PORT"),
+    }
+
+    if config.Port == "" {
+        config.Port = "8000"
+    }
+
+    return config, nil
 }
