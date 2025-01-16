f72b748ac5ddac8e94716f02d539d1c8c8f7e97d
Asif Rahaman <asifrahaman162@gmail.com>
2025-01-16 18:17:58 +0530

    wip: fix the width of the dropdowns

--- frontend/src/app/components/ui/Dropdown.tsx
+++ frontend/src/app/components/ui/Dropdown.tsx
@@ -39,7 +39,7 @@ export default function DropDownBox() {
   return (
     <Listbox value={selected} onChange={handleSelectionChange}>
       <div className="relative">
-        <Listbox.Button className="grid w-full cursor-default grid-cols-1 rounded-md bg-white py-1.5 pl-3 pr-2 text-left text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+        <Listbox.Button className="grid w-48 cursor-default grid-cols-1 rounded-md bg-white py-1.5 pl-3 pr-2 text-left text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
           <span className="col-start-1 row-start-1 flex items-center gap-3 pr-6">
             <span className="block truncate">{selected.name}</span>
           </span>
@@ -51,7 +51,7 @@ export default function DropDownBox() {
 
         <Listbox.Options
           transition
-          className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm"
+          className="absolute z-10 mt-1 max-h-56 w-64 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm"
         >
           {people.map((person) => (
             <Listbox.Option
